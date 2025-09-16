cbb3973610e9becee37b4d1d0211b8467aa5223e
usmanovdenis <usmanovimpulsem@yandex.ru>
2025-09-16 18:12:28 +0500

    prepare to delete instance test

--- src/app/entry.js
+++ src/app/entry.js
@@ -1,6 +1,10 @@
 import {
     AxesHelper,
+    BoxGeometry,
     GridHelper,
+    InstancedMesh,
+    Matrix4,
+    MeshBasicMaterial,
     PerspectiveCamera,
     Scene,
     Vector3,
@@ -8,7 +12,6 @@ import {
 } from "three";
 import { OrbitControls, SimplexNoise } from "three/examples/jsm/Addons.js";
 import { degToRad } from "three/src/math/MathUtils.js";
-
 
 export default async function () {
     const scene = new Scene();
@@ -26,12 +29,12 @@ export default async function () {
 
     let chunkScale = 4;
 
-    camera.position.x = 0
+    camera.position.x = 0;
     camera.position.y = chunkScale / 2;
-    camera.position.z = 1
-    
+    camera.position.z = 1;
+
     const controls = new OrbitControls(camera, renderer.domElement);
-    
+
     controls.target.set(0, 0, 0);
     controls.update();
 
@@ -39,16 +42,36 @@ export default async function () {
         renderer.domElement.clientWidth / renderer.domElement.clientHeight;
     camera.updateProjectionMatrix();
 
-
     const gridHelper = new GridHelper(50, 50);
     scene.add(gridHelper);
 
     const axesHelper = new AxesHelper(50);
-    axesHelper.setColors('red', 'yellow', 'lime');
+    axesHelper.setColors("red", "yellow", "lime");
     scene.add(axesHelper);
 
+    const matrix = new Matrix4();
+    let instance = new InstancedMesh(
+        new BoxGeometry(1, 1, 1),
+        new MeshBasicMaterial({ color: "green" }),
+        3
+    );
+    instance.count = 1;
 
-   
+    matrix.setPosition(1, 0, 0);
+    instance.setMatrixAt(instance.count++, matrix);
+
+    matrix.setPosition(2, 0, 0);
+    instance.setMatrixAt(instance.count++, matrix);
+
+    scene.add(instance);
+
+    window.addEventListener("keyup", (event) => {
+        if (event.key === "]") {
+            
+            console.log(mesh)
+        }
+    });
+
     function animate() {
         //   cube.rotation.x += 0.01;
         //   cube.rotation.y += 0.01;
